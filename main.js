cd7db731713beca3ea24a9983de9a5c748fbea56
JimmyLin19950412 <42556633+JimmyLin19950412@users.noreply.github.com>
2019-03-12 14:14:04 -0500

    Initial files/UI design

---
+++ main.js
@@ -0,0 +1,331 @@
+//array that hold nodes x position, starts empty
+var nodesX = [];
+//array that hold nodes y position, starts empty
+var nodexY = [];
+//array that hold anchor node positions. 0 = not anchor nodes, 1 = is anchor nodes
+var anchorNodes = [];
+
+//JSON object holding nodes info
+var jsonNodes = {
+    nodes: []
+};
+
+//JSON object holding drawing info
+var jsonDraw = {
+    draw: []
+};
+
+//boolean to control if drawing is enabled. Starts false, enables when pen button is clicked
+var toggleDraw = false;
+//boolean flag determining if user is drawing. originally, false
+var flag = false;
+//previous and current positions of mouse, maintined to draw properly
+var prevX = 0;
+var currX = 0;
+var prevY = 0;
+var currY = 0;
+var dot_flag = false;
+
+//resizes canvas size
+function resizeCanvas() {
+    /*
+    //resize the width of canvas
+    document.getElementById("canvas").width = window.innerWidth - 20;
+    //resize the height of canvas
+    document.getElementById("canvas").height = window.innerHeight - 55;
+    */
+
+    document.getElementById("canvas").width = document.getElementById("sensorFieldWidthSlider").value;
+    document.getElementById("canvas").height = document.getElementById("sensorFieldLengthSlider").value;
+
+}
+
+//opens modal Generate WSNs
+function openGenerateWSNs() {
+    document.getElementById("generateWSNsModal").style.display="block";
+}
+
+//closes modeal Generate WSNs
+function closeGenerateWSNs() {
+    document.getElementById("generateWSNsModal").style.display="none";
+}
+
+//get value of penWidth
+function penWidthGetValue() {
+    document.getElementById("penWidthValue").innerHTML = document.getElementById("penWidthSlider").value;
+}
+
+//get value of nodes in network slider
+function nodesInNetworkGetValue() {
+    document.getElementById("nodesInNetworkValue").innerHTML = document.getElementById("nodesInNetworkSlider").value;
+}
+
+//get value of anchors in network slider
+function anchorsInNetworkGetValue() {
+    document.getElementById("anchorsInNetworkValue").innerHTML = document.getElementById("anchorsInNetworkSlider").value;
+}
+//get value of raidio range slider
+function radioRangeGetValue() {
+    document.getElementById("radioRangeValue").innerHTML = document.getElementById("radioRangeSlider").value;
+}
+//get value of sensor field width slider
+function sensorFieldWidthGetValue() {
+    document.getElementById("sensorFieldWidthValue").innerHTML = document.getElementById("sensorFieldWidthSlider").value;
+}
+
+//get value of sensor field length slider
+function sensorFieldLengthGetValue() {
+    document.getElementById("sensorFieldLengthValue").innerHTML = document.getElementById("sensorFieldLengthSlider").value;
+}
+
+//changes toggleDraw to true or false depending on button click
+function toggleDrawing() {
+    //if toggleDraw is false then changes toggleDraw to true
+    if(toggleDraw == false) {
+        toggleDraw = true;
+        //change color of button to show that toggleDraw is true/on
+        document.getElementById("penButton").style.background = "#FF0000";
+    }
+    //if toggleDraw is not false then it is true, change toggle draw to false
+    else {
+        toggleDraw = false;
+        //changes color of button to show that toggleDraw is false/off
+        document.getElementById("penButton").removeAttribute("style");
+    }
+}
+
+//reset variables from generateNodes
+function resetGenerateNodes() {
+    //reset nodesX and nodesY array
+    nodesX = [];
+    nodesY = [];
+    //reset anchorNodes. 0 = not anchor nodes, 1 = is anchor nodes
+    for(var i = 0; i < document.getElementById("anchorsInNetworkSlider").value; i++) {
+        //change current position of array to 0
+        anchorNodes[i] = 0;
+    }
+    //determine anchorNodes. # of anchor nodes determined by slider in Generate WSNs
+    for(var i = 0; i < document.getElementById("anchorsInNetworkSlider").value; i++) {
+        //generate a random number from 0 to length of nodesX or nodesY
+        var ran = Math.floor(Math.random() * nodesX.length);
+            
+        //see if duplicate ran is found
+        //if position of array is not 0 then a duplicate is found
+        if(anchorNodes[i] == 0) {
+            anchorNodes[i] = 1;
+        }
+        else {
+            //reduce the value of i by 1, effectively negating the loops iteration
+            i--
+        }
+    }
+    //reset jsonNodes
+    var jsonNodes = {
+        nodes: []
+    };
+}
+
+//generate nodes
+function generateNodes() {
+    //get # of nodes from Generate WSNs
+    var numNodes = document.getElementById("nodesInNetworkSlider").value;
+    //get X value of canvas from Generate WSNs
+    var maxX = document.getElementById("sensorFieldWidthSlider").value;
+    //get Y value of canvas from Generate WSNs
+    var maxY = document.getElementById("sensorFieldLengthSlider").value;
+
+    //loops until length of nodes = numNodes
+    //generates x and y coordinates of nodes
+    while(nodesX.length < numNodes && nodesY.length < numNodes) {
+        //generate random x (0 - maxX.length)
+        var ranX = Math.floor(Math.random() * maxX);
+        //generate random y (0 - maxY.length)
+        var ranY = Math.floor(Math.random() * maxY);
+
+        //check to see if combination of x and y positions are duplicates
+        //nodesX and nodesY are/should be same length, checking either length is enough
+        //counter for for loop
+        var i = 0;
+        for(i = 0; i < nodesX.length; i++) {
+            //if position is duplicate
+            if(nodesX[i] == ranX && nodesY[i] == ranY) {
+                //break out for loop
+                break;
+            }
+        }
+
+        //if i is = to nodesX or nodesY length then that means for loop reached end of arrays, meaning no duplicates
+        if(i == nodesX.length) {
+            //add generates x and y to position of nodes
+            nodesX.push(ranX);
+            nodesY.push(ranY);
+        }
+    }
+}
+
+//place generated nodes on canvas
+function placeNodesOnCanvas() {
+    //get canvas element from html
+    var canvas = document.getElementById("canvas");
+    //canvas is 2d
+    var ctx = canvas.getContext("2d");
+
+    //loops through length of nodesX and nodesY array
+    for(var i = 0; i < nodesX.length; i++) {
+        //if current node is an anchor node
+        if(anchorNodes[i] == 1) {
+            //change color of nodes to red
+            ctx.fillStyle = "#FF0000";   
+        }
+        //if current node is not an anchor node
+        else {
+            //change color of nodes to black
+            ctx.fillStyle = "#000000";
+        }
+
+        //place nodes on canvas
+        //ctx.fillRect(nodesX[i], nodesY[i], 5, 5);
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.arc(nodesX[i], nodesY[i], 1, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+//creates json with node information
+function createJsonNodes() {
+    //loops through nodesX / nodesY
+    for(var i = 0; i < nodesX.length; i++) {
+        //variable containing data if current node is an anchor, default is false.
+        var isAnchor = false;
+
+        //determine if current node i is an anchor node
+        if(anchorNodes[i] == 1) {
+            //current node is anchor
+            isAnchor = true;
+        }
+
+        //push data into jsonNodes
+        jsonNodes.nodes.push({
+            "NodeID" : i++,
+            "X" : nodesX[i],
+            "Y" : nodesY[i],
+            "IsAnchor" : isAnchor
+        });
+    }
+}
+
+//tracks mouse movement
+function trackMouse() {
+    var canvas = document.getElementById("canvas");
+
+    //track the users mouse and actions
+    canvas.addEventListener("mousemove", function (e) {
+        findXY("move", e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findXY("down", e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findXY("up", e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findXY("out", e)
+    }, false);
+}
+
+//finds x and y coordinate of mouse
+function findXY(action, e) {
+    if(toggleDraw == true) {
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+
+        //if user mouse is down
+        if(action == "down") {
+            //change prev x and y locations to current x and y locations
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.getBoundingClientRect().left;
+            currY = e.clientY - canvas.getBoundingClientRect().top;
+            //drawing is true, flag is true
+            flag = true;
+            dot_flag = true;
+
+            //starting position of drawing/line
+            if(dot_flag == true) {
+                //start drawing
+                ctx.beginPath();
+                //change color of nodes to black
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(currX, currY, 1, 1);
+                //add starting point to json object
+                jsonDraw.draw.push({
+                    "X" : currX,
+                    "Y" : currY
+                });
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+
+        //if user lifts up mouse button or leaves canvas, stop drawing
+        if (action == "up" || action == "out") {
+            flag = false;
+        }
+
+        //if user moves mouse, draw lines from starting point
+        if (action == "move") {
+            if(flag == true) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.getBoundingClientRect().left;
+                currY = e.clientY - canvas.getBoundingClientRect().top;
+                draw();
+            }
+        }
+    }
+}
+
+//allows the user to draw on canvas
+function draw() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    //start drawing
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    jsonDraw.draw.push({
+        "X" : currX,
+        "Y" : currY
+    });
+    //change color of nodes to black
+    ctx.fillStyle = "#000000";
+    ctx.lineWidth = document.getElementById("penWidthSlider").value;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+//erases everything in canvas then places generated nodes back on canvas
+function erase() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext('2d');
+    
+    //clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //reset json object
+    var jsonDraw = {
+        draw: []
+    };
+    //place nodes back on canvas
+    placeNodesOnCanvas();
+}
+
+//clears entire canvas
+function clearCanvas() {
+    //resets all variables
+    resetGenerateNodes();
+    erase();
+}
